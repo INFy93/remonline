2f32074a2402176f4ea10c9f092eef2ded0abd10
INFy93 <infy93@gmail.com>
2022-04-05 09:35:50 +0300

    add check order status view

--- app/Http/Controllers/CheckOrderController.php
+++ app/Http/Controllers/CheckOrderController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CheckOrderController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('client.checking_order_status');
+    }
 }
